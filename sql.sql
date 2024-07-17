87de026a1a4a640dfec7599940e10457eec0928f
Aayush Raj <143957572+Aayushraj72@users.noreply.github.com>
2024-07-17 16:06:05 +0530

    Add files via upload

---
+++ sql.sql
@@ -0,0 +1,11 @@
+create database Quiz_Application;
+use Quiz_Application;
+
+show databases;
+create table Login(EmailId varchar(50), UserId  varchar(50),  Password varchar(50)  );
+select * from Login;
+ drop table Login;
+ 
+ create table SignUpOne(UserId  varchar(50),  Password varchar(50), UserName varchar(60), EmailId varchar(60), PhNo varchar(50), DOB varchar(50), Sex varchar(50) );
+drop table SignUpOne;
+Select * from SignUpOne;
